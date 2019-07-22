47bb99a53717884e12209cf8d426c0504b033747
RAY <RAY@192.168.0.109>
2019-07-22 15:21:09 +0900

    initial commit

---
+++ src/민욱이도전.java
@@ -0,0 +1,7 @@
+
+public class 민욱이도전 {
+	public static void main(String[] args) {
+		System.out.println("민욱이   바보야");
+	}
+
+}
---
+++ src/민욱이도전1.java
@@ -0,0 +1,3 @@
+public class 민욱이도전1 
+{public static void main(String[] args) 
+{System.out.println("민욱이세번째도전");}}
